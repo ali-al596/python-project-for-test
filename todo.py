a3b5e46d497627f02417bfadef4270ed8046535a
Ali <alialmasi93113@gmail.com>
2025-09-06 22:51:05 +0330

    this my firs commit

---
+++ todo.py
@@ -0,0 +1,27 @@
+tasks = []
+
+while True :
+    print("===To Do List---")
+    print("1: Add new tasks")
+    print("2: show tasks")
+    print("3: exit")
+
+    choice = input("please choose")
+
+
+
+    if choice == "1" :
+        task = input('enter your new task')
+        tasks.append(task)
+        print("task added")
+    elif choice == "2" :
+        if not  tasks:
+            print("this is empty...!")
+        else :
+            for i , t in enumerate(tasks,1):
+                print(f"{i}, {t}")
+    elif choice == "3":
+        print("bye")
+        break
+    else:
+        prakint("not found")      
