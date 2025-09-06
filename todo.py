55766fc28bd48a36483ca695dc2f85d6547c7268
Ali <alialmasi93113@gmail.com>
2025-09-06 14:49:10 +0330

    this is test second commit todo

--- todo.py
+++ todo.py
@@ -1,24 +1,2 @@
-
-works = []
-
-print("---To Do List---")
-print('1: add new work')
-print("2: show works ")
-print("3: exit")
-
-selected_number = input("choose")
-
-if selected_number == 1 :
-    work = input("Add your new work")
-    works.append(work)
-    print('work added')
-
-elif selected_number == 2 :
-    if not works:
-        print("no work added")
-    else :
-        print(works)
-
-elif selected_number == 3 :
-    print("goodbye")
-    
+print('hello')
+print("wvwr")
